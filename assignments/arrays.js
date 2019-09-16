30abc52bf87009ca217c812965e71ab76085cc31
Jessica Cooper <jessicagcooper@gmail.com>
2019-09-16 18:09:07 -0500

    alterned Array assignment to replace for loops with usage of .forEach in challenge 3  and .map in challenge 4.

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -92,19 +92,27 @@ console.log(lastCar.car_make, lastCar.car_model);
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
 let carModelsSorted = [];
-for (let i = 0; i < inventory.length; i++){
+/*for (let i = 0; i < inventory.length; i++){
   carModels.push(inventory[i].car_model);
   carModelsSorted = carModels.sort();
-}
-
+}*/
+inventory.forEach(function(element) {
+  carModels.push(element.car_model);
+  return carModels;
+})
+carModelsSorted = carModels.sort();
 console.log(carModelsSorted);
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
-for (let i = 0; i < inventory.length; i++){
+/*for (let i = 0; i < inventory.length; i++){
   carYears.push(inventory[i].car_year);
-}
+}*/
+inventory.map(function(element) {
+  carYears.push(element.car_year); 
+  return carYears;
+})
 
 console.log(carYears);
 
